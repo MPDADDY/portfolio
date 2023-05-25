9537b37200bcf5a5b6f72705e3fb298605ed018e
anold <anoldmwawasi@gmail.com>
2023-05-25 21:49:04 +0300

    added a validation script to the formvalidation.js

--- formvalidation.js
+++ formvalidation.js
@@ -1,3 +1,17 @@
 const emailErr = document.getElementById('emailErr');
 const submit = document.getElementById('submit');
 emailErr.style.color = 'red';
+function emailValidation() {
+  const email = document.getElementById('email').value;
+  if (email.toLowerCase() !== email) {
+    emailErr.innerHTML = 'Input the email in lowercase';
+  }
+  return false;
+}
+
+submit.addEventListener('click', (event) => {
+  if (!emailValidation()) {
+    event.preventDefault();
+  }
+});
+submit.addEventListener('click', emailValidation);
