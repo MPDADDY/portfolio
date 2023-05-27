f2f761b7a57b2118b94ed459a588bc0c347a29c0
Omotolani Akinmola <akinmolaomotolani@gmail.com>
2023-05-27 07:22:25 +0100

    commit for the local storage

--- localstorage.js
+++ localstorage.js
@@ -1,3 +1,23 @@
 const name = document.getElementById('name').value;
 let email = document.getElementById('email').value;
 let message = document.getElementById('comments').value;
+
+popupModal.forEach((popup) => {
+    const popupheader = document.createElement('h3');
+    popupheader.classList = 'heading-secondary';
+    popupheader.innerHTML = popup.headingSecondary;
+  
+    const closeButton = document.createElement('button');
+    closeButton.innerHTML = popup.button;
+    closeButton.classList = 'btn-close';
+  
+    const textContainer = document.createElement('div');
+    textContainer.className = 'text-container-work';
+  
+    const subheading = document.createElement('p');
+    subheading.innerHTML = popup.subheading;
+    subheading.className = 'subheading';
+  
+    const dot = document.createElement('span');
+    dot.innerHTML = popup.dot;
+    dot.className = 'dot';
