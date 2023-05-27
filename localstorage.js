4e688beb2c1b01d034f7558beb59cd79e626e427
anold <anoldmwawasi@gmail.com>
2023-05-27 14:34:33 +0300

    resolved issues with html

--- localstorage.js
+++ localstorage.js
@@ -21,4 +21,28 @@ window.onload = () => {
     email.value = data.email;
     userMessage.value = data.message;
   }
+}; 
+
+const userMessage = document.getElementById('comments');
+const form = document.getElementById('contact_form');
+
+const formInputsData = {
+  name: '',
+  email: '',
+  message: '',
 };
+form.addEventListener('input', () => {
+  formInputsData.name = userName.value;
+  formInputsData.email = email.value;
+  formInputsData.message = userMessage.value;
+  localStorage.setItem('formInputsData', JSON.stringify(formInputsData));
+});
+window.onload = () => {
+  const data = JSON.parse(localStorage.getItem('formInputsData')); // Corrected key name
+  if (data) {
+    userName.value = data.name; // Use the retrieved data instead of formInputsData
+    email.value = data.email;
+    userMessage.value = data.message;
+  }
+};
+ 
